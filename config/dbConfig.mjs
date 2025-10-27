616929b842132bac0a3ba4cf6c10cd40487bd2fa
Antoheredia28 <antonellaherediaia@gmail.com>
2025-10-27 15:25:55 -0300

    nuevos comentarios

--- config/dbConfig.mjs
+++ config/dbConfig.mjs
@@ -1,5 +1,7 @@
+//Importa la libreria mongoose
 import mongoose from 'mongoose';
 
+//exporta la funcion a app.mjs para poder conectarse a la base de datos
 export async function connectDB(){
     try{
         await mongoose.connect('mongodb+srv://Grupo-20:grupo20@cursadanodejs.ls9ii.mongodb.net/Node-js');
